eb22bba0012bf2a19ba4392676d0df8e07586a70
Ben Bubnick <ben.bubnick@gmail.com>
2015-04-06 04:12:31 -0400

    Create random.cs

---
+++ random.cs
@@ -0,0 +1,34 @@
+private static double randomNormal()
+{
+	return Math.Cos(2 * Math.PI * tRand.NextDouble()) * Math.Sqrt(-2 * Math.Log(tRand.NextDouble()));
+}
+ 
+//Then calculate the average and the Standard Deviation:
+ 
+static Random tRand = new Random();
+ 
+static void Main(string[] args)
+{
+	double[] a = new double[1000];
+ 
+	double tAvg = 0;
+	for (int x = 0; x < a.Length; x++)
+	{
+		a[x] = randomNormal() / 2 + 1;
+		tAvg += a[x];
+	}
+ 
+	tAvg /= a.Length;
+	Console.WriteLine("Average: " + tAvg.ToString());
+ 
+	double s = 0;
+	for (int x = 0; x < a.Length; x++)
+	{
+		s += Math.Pow((a[x] - tAvg), 2);
+	}
+	s = Math.Sqrt(s / 1000);
+ 
+	Console.WriteLine("Standard Deviation: " + s.ToString());
+ 
+	Console.ReadLine();
+}
